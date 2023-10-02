3d1e8f514371e5d47da62d2090596e054fa609aa
zrx <zrx@zrxcomp.sozonov.tech>
2023-10-03 00:05:20 +0300

    Porting to TS

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,61 @@
+import { Request, Response } from "express"
+
+//{{{ Constants
+
+const appSubfolder = "blog/"
+const ingestSubfolder: String = "_ingest/"
+const ingestCoreSubfolder: String = "_ingestCore/"
+const docsSubfolder = "_d/"
+const mediaSubfolder = "_m/"
+const scriptsSubfolder = "_s/"
+const globalsSubfolder = "_g/"
+const coreSubfolder = "_core/"
+const updateFreq = 300 // seconds before the cache gets rescanned
+
+//}}}
+//{{{ Blog core
+
+class Blog {
+    constructor(private docCache: DocCache, 
+                private navTopic: NavTree,
+                private navTime: NavTree,
+                private updated: Date
+               ){}
+
+    checkNUpdate() {
+        const now = new Date()
+        if (now - updated < updateFreq) {
+            return
+        }
+        mutex {
+            docCache.ingestAndRefresh()
+            const navTrees = buildNavTrees(docCache)
+            navTopic = navTrees.f1
+            navTime = navTrees.f2
+            updated = new Date()
+        }
+    }
+}
+
+//}}}
+//{{{ Document cache
+//}}}
+//{{{ Web app
+
 const express = require( "express" );
 const app = express();
 const port = 8000; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+app.get( "/", ( req: Request, res: Response ) => {
     res.send( "Hello world!" );
-} );
+});
 
 // start the Express server
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
-} );
+});
 
+//}}}
+//{{{ Utils
+//}}}
