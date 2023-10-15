2df292e9b72789d95ed76088ede28efe7ee82db2
zrx <zrx@zrxcomp.sozonov.tech>
2023-10-15 21:06:49 +0300

    Java

--- test/test.java
+++ test/test.java
@@ -1,3 +1,4 @@
+//{{{ Imports
 package site.sozonov.blog.test;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,7 +13,8 @@ import java.util.Collections;
 import java.util.function.Predicate;
 import java.util.function.Function;
 import java.lang.reflect.Array;
-
+import java.nio.file.Paths;
+//}}}
 class Test {
 
 interface FileSys {
@@ -20,6 +22,7 @@ interface FileSys {
    L<FileInfo> listFiles(String dir);
    L<String> listDirs(String dir);
    String readTextFile(String dir, String fN);
+   boolean createDir(String dir);
    boolean saveOverwriteFile(String dir, String fN, String cont);
    boolean moveFile(String dir, String fN, String targetDir);
    String moveFileToNewVersion(String dir, String fN, String targetDir);
@@ -31,28 +34,29 @@ interface FileSys {
 static class L<T> implements List<T> {
    private static final int DEFAULT_CAPACITY = 10;
    private int size;
-   private int modCount;
+   private int modCount = 0;
    private T[] data;
-   
+
    public L(int initCap) {
       if (initCap < 0) {
-         throw new IllegalArgumentException();
-      } 
+         throw new RuntimeException();
+      }
       data = (T[]) new Object[initCap];
    }
 
    public L() {
       this(4);
    }
-   
+
    public static <T> L<T> of(T... values)  {
-      L<T> result = new L(values.length); 
+      L<T> result = new L(values.length);
       for (int i = 0; i < values.length; i++) {
          result.data[i] = values[i];
       }
+      result.size = values.length;
       return result;
    }
-   
+
    public int size()  {
       return size;
    }
@@ -60,65 +64,65 @@ static class L<T> implements List<T> {
    public boolean isEmpty() {
       return size == 0;
    }
-   
+
    public boolean notEmpty()  {
       return size > 0;
    }
-   
+
    public int indexOf(Object e) {
       return -1;
    }
- 
+
    public int lastIndexOf(Object e) {
       return -1;
    }
-   
+
    public Object[] toArray() {
       T[] array = (T[]) new Object[size];
       System.arraycopy(data, 0, array, 0, size);
       return array;
    }
-   
+
    public <T> T[] toArray(T[] a) {
       if (a.length < size) {
          a = (T[]) Array.newInstance(a.getClass().getComponentType(), size);
       } else if (a.length > size) {
          a[size] = null;
-      } 
+      }
       System.arraycopy(data, 0, a, 0, size);
       return a;
    }
-   
+
    public T get(int index) {
       checkBoundExclusive(index);
       return data[index];
    }
- 
+
    public T set(int index, T e) {
       checkBoundExclusive(index);
       T result = data[index];
       data[index] = e;
       return result;
    }
- 
+
    public boolean add(T e) {
       modCount++;
       if (size == data.length) {
          ensureCapacity(size + 1);
-      } 
+      }
       data[size++] = e;
       return true;
    }
- 
+
    public void add(int index, T e) {
       checkBoundInclusive(index);
       modCount++;
-      if (size == data.length) { ensureCapacity(size + 1); } 
-      if (index != size) { System.arraycopy(data, index, data, index + 1, size - index); } 
+      if (size == data.length) { ensureCapacity(size + 1); }
+      if (index != size) { System.arraycopy(data, index, data, index + 1, size - index); }
       data[index] = e;
       size++;
    }
- 
+
    public T remove(int index) {
       checkBoundExclusive(index);
       T r = data[index];
@@ -128,7 +132,7 @@ static class L<T> implements List<T> {
       data[size] = null;
       return r;
    }
- 
+
    public void clear() {
       if (size == 0) {
          return;
@@ -138,26 +142,26 @@ static class L<T> implements List<T> {
       Arrays.fill(data, 0, size, null);
       size = 0;
    }
-   
+
    public void ensureCapacity(int minCapacity) {
       int current = data.length;
- 
+
       if (minCapacity > current) {
          T[] newData = (T[]) new Object[Math.max(current * 2, minCapacity)];
          System.arraycopy(data, 0, newData, 0, size);
          data = newData;
       }
    }
- 
+
    public boolean addAll(Collection<? extends T> c) {
       return addAll(size, c);
    }
- 
+
    public boolean addAll(int index, Collection<? extends T> c) {
       checkBoundInclusive(index);
       Iterator<? extends T> itr = c.iterator();
       int csize = c.size();
-  
+
       modCount++;
       if (csize + size > data.length) { ensureCapacity(size + csize); }
       int end = index + csize;
@@ -165,109 +169,55 @@ static class L<T> implements List<T> {
       size += csize;
       for ( ; index < end; index++) {
          data[index] = itr.next();
-      } 
+      }
       return csize > 0;
    }
-   
-   @Override 
+
+   @Override
    public List<T> subList(int fromIndex, int toIndex) {
       return null;
    }
-   
-   @Override 
+
+   @Override
    public ListIterator<T> listIterator() {
-      return null;
+      return listIterator(0);
    }
-   
-   @Override 
+
+   @Override
    public ListIterator<T> listIterator(final int i) {
-      return null; 
-//~      checkForComodification();
-//~      rangeCheckForAdd(index);
-//~
-//~      return new ListIterator<T>() {
-//~         private final ListIterator<T> i = l.listIterator(index+offset);
-//~
-//~         public boolean hasNext() {
-//~            return nextIndex() < size;
-//~         }
-//~
-//~         public T next() {
-//~            if (hasNext()) {
-//~               return i.next();
-//~            } else {
-//~               throw new NoSuchElementException();
-//~            } 
-//~         }
-//~
-//~         public boolean hasPrevious() {
-//~            return previousIndex() >= 0;
-//~         }
-//~
-//~         public T previous() {
-//~            if (hasPrevious()) {
-//~               return i.previous();
-//~            } else {
-//~               throw new NoSuchElementException();
-//~            } 
-//~         }
-//~
-//~         public int nextIndex() {
-//~            return i.nextIndex() - offset;
-//~         }
-//~
-//~         public int previousIndex() {
-//~            return i.previousIndex() - offset;
-//~         }
-//~
-//~         public void remove() {
-//~            i.remove();
-//~            SubList.this.modCount = l.modCount;
-//~            size--;
-//~         }
-//~
-//~         public void set(L e) {
-//~            i.set(e);
-//~         }
-//~
-//~         public void add(L e) {
-//~            i.add(e);
-//~            SubList.this.modCount = l.modCount;
-//~            size++;
-//~         }
-//~      };
+      return new LIterator<T>(this);
    }
-   
-   @Override 
+
+   @Override
    public Iterator<T> iterator() {
       return listIterator();
    }
-   
-   @Override 
+
+   @Override
    public boolean retainAll(Collection<?> c) {
       return false;
    }
- 
-   @Override 
+
+   @Override
    public boolean contains(Object o) {
       return false;
    }
-   
-   @Override 
+
+   @Override
    public boolean removeAll(Collection<?> c) {
       return false;
    }
-   
-   @Override 
+
+   @Override
    public boolean remove(Object o) {
       return false;
    }
-   
-   @Override 
+
+   @Override
    public boolean containsAll(Collection<?> c) {
       return false;
    }
-   
+
    public void removeRange(int fromIndex, int toIndex) {
       int change = toIndex - fromIndex;
       if (change > 0) {
@@ -275,80 +225,82 @@ static class L<T> implements List<T> {
          System.arraycopy(data, toIndex, data, fromIndex, size - toIndex);
          size -= change;
       } else if (change < 0) {
-         throw new IndexOutOfBoundsException();
-      } 
+         throw new RuntimeException();
+      }
    }
- 
+
    private void checkBoundInclusive(int index) {
       // Implementation note: we do not check for negative ranges here, since
-      // use of a negative index will cause an ArrayIndexOutOfBoundsException,
+      // use of a negative index will cause a RuntimeException,
       // a subclass of the required exception, with no effort on our part.
       if (index > size) {
-         throw new IllegalArgumentException("Index: " + index + ", Size: " + size);
-      } 
+         throw new RuntimeException("Index: " + index + ", Size: " + size);
+      }
    }
- 
+
    private void checkBoundExclusive(int index) {
       // Implementation note: we do not check for negative ranges here, since
-      // use of a negative index will cause an ArrayIndexOutOfBoundsException,
+      // use of a negative index will cause a RuntimeException,
       // a subclass of the required exception, with no effort on our part.
       if (index >= size) {
-          throw new IllegalArgumentException("Index: " + index + ", Size: " + size);
-      } 
+          throw new RuntimeException("Index: " + index + ", Size: " + size);
+      }
    }
- 
+
    boolean removeAllInternal(Collection<?> c) {
       int i;
       int j;
       for (i = 0; i < size; i++) {
          if (c.contains(data[i])) {
             break;
-         } 
-      } 
+         }
+      }
       if (i == size) {
          return false;
-      } 
-  
+      }
+
       modCount++;
       for (j = i++; i < size; i++) {
          if (!c.contains(data[i])) {
             data[j++] = data[i];
-         } 
-      } 
+         }
+      }
       size -= i - j;
       return true;
    }
- 
+
    public int findIndex(Function<T, Boolean> pred)  {
       for (int i = 0; i < size; i++) {
          if (pred.apply(data[i])) {
             return i;
          }
       }
-      return -1; 
+      return -1;
    }
-   
+
    public <U> L<U> trans(Function<T, U> transformer) {
       L<U> result = new L(this.size);
       for (int i = 0; i < size; i++) {
          result.data[i] = transformer.apply(data[i]);
       }
+      result.size = this.size;
       return result;
    }
-   
+
    public <U> L<U> transIf(Predicate<T> pred, Function<T, U> transformer) {
       L<U> result = new L(this.size);
-      int j = 0; 
+      int j = 0;
       for (int i = 0; i < size; i++) {
-         T elt = data[i]; 
+         T elt = data[i];
          if (pred.test(elt)) {
-            result.data[j] = transformer.apply(elt); 
+            result.data[j] = transformer.apply(elt);
             j++;
          }
       }
+      result.size = j;
       return result;
    }
-   
+
    public Optional<T> first(Predicate<T> pred)  {
       for (int i = 0; i < size; i++) {
          if (pred.test(data[i])) {
@@ -357,18 +309,87 @@ static class L<T> implements List<T> {
       }
       return Optional.empty();
    }
+
+   public static class LIterator<T> implements ListIterator<T> {
+      private int i;
+      private int size;
+      private L<T> lst;
+      private int expectedModCount;
+
+      public LIterator(L<T> lst) {
+         this.i = -1;
+         this.size = lst.size;
+         this.lst = lst;
+         expectedModCount = lst.modCount;
+      }
+
+      public boolean hasNext() {
+         return i < size - 1;
+      }
+
+      public T next() {
+         if (i < size - 1) {
+            i++;
+            return lst.data[i];
+         } else {
+            throw new RuntimeException();
+         }
+      }
+
+      public boolean hasPrevious() {
+         return i > 0;
+      }
+
+      public T previous() {
+         if (i > 0) {
+            i--;
+            return lst.data[i];
+         } else {
+            throw new RuntimeException();
+         }
+      }
+
+      public int nextIndex() {
+         return i + 1;
+      }
+
+      public int previousIndex() {
+         return i - 1;
+      }
+
+      public void remove() {
+         lst.remove(i);
+         lst.modCount++;
+      }
+
+      public void set(T e) {
+         lst.set(i, e);
+      }
+
+      public void add(T e) {
+         lst.add(e);
+         expectedModCount++;
+      }
+
+      final void checkForComodification() {
+         if (lst.modCount != expectedModCount) {
+            throw new RuntimeException();
+         }
+      }
+   }
 }
+
 
 //}}}
 //{{{ Constants
 
-static final String staticDir = "/var/www/blst";
+static final String blogDir = "/var/www/blst";
 static final String ingestDir = "/var/www/blog";
 
 static final String appSuburl = "blog/";
 static final String coreSubfolder = "_c/";
 static final int updateFreq = 300; // seconds before the cache gets rescanned
-static final L<String> coreFiles = 
+static final L<String> coreFiles =
    L.of("notFound.png", "notFound.html", "core.css", "core.html", "core.js",
            "favicon.ico", "footer.html", "no.png", "yes.png", "termsOfUse.html");
 
@@ -407,6 +428,15 @@ static class MockFileSys implements FileSys {
          return "";
       }
       return fs.get(dir).first(x -> x.name.equals(fN)).map(x -> x.cont).orElse("");
+   }
+
+   @Override
+   public boolean createDir(String dir) {
+      if (fs.containsKey(dir)) {
+         return true;
+      }
+      fs.put(dir, new L());
+      return true;
    }
 
    @Override
@@ -449,6 +479,8 @@ static class MockFileSys implements FileSys {
       var sourceFiles = fs.get(dir);
       int indexSource = sourceFiles.findIndex(x -> x.name.equals(fN));
       MockFile sourceFile = sourceFiles.get(indexSource);
+
+      createDir(targetDir);
       var targetFiles = fs.get(targetDir);
 
       var existingWithThisPrefix = getNamesWithPrefix(targetDir, shaveOffExtension(fN));
@@ -485,8 +517,10 @@ static class MockFileSys implements FileSys {
          return new L();
       }
       var existingFiles = fs.get(dir);
-      return existingFiles.transIf(x -> x.name.startsWith(prefix), y -> y.name);
+      var tra = existingFiles.transIf(x -> x.name.startsWith(prefix), y -> y.name);
+      return tra;
    }
+
 }
 
 
@@ -510,9 +544,6 @@ static class MockFile {
       this.modified = modified;
    }
 }
-
-
-
 
 //}}}
 //{{{ Utils
@@ -551,7 +582,7 @@ static Optional<Integer> parseInt(String s) {
 static void blAssert(boolean predicate) {
    if (!predicate) {
       String methodName = getFuncName(3);
-      throw new IllegalArgumentException(methodName);
+      throw new RuntimeException(methodName);
    }
 }
 
@@ -566,7 +597,7 @@ static String getFuncName(int depth) {
 
 static void assertArrsEqual(L<String> a, L<String> b) {
    if(a.size() != b.size()) {
-      throw new IllegalArgumentException("Arrays have different lengths");
+      throw new RuntimeException("Arrays have different lengths");
    } else if (a.size() == 0) {
       return;
    }
@@ -574,7 +605,7 @@ static void assertArrsEqual(L<String> a, L<String> b) {
    Collections.sort(b);
    for(int i = 0; i < a.size(); i++) {
       if(a.get(i) != b.get(i)) {
-         throw new IllegalArgumentException(
+         throw new RuntimeException(
                "Arrays differ at " + i + ", \"" + a.get(i) + "\" vs \"" + b.get(i) + "\"");
       }
    }
@@ -590,17 +621,16 @@ boolean isFixedCore(String fN) {
    return false;
 }
 
-static String getCoreDir() {
-   return ingestDir + coreSubfolder;
+static String getIngestDir() {
+   return Paths.get(ingestDir, coreSubfolder).toString();
 }
-
 
 static void runTest(Action theTest, TestResults counters) {
    try {
       theTest.run();
    } catch (Exception e) {
-      System.out.println("Failed: " + e.getMessage()); 
-      //e.printStackTrace(); 
+      System.out.println("Failed: " + e.getMessage());
+      e.printStackTrace();
       counters.countFailed++;
    }
    counters.countRun++;
@@ -612,7 +642,7 @@ static int getMaxVersion(L<String> filenames) {
    var versions = new L();
    int result = 1;
    for (int i = 0; i < filenames.size(); i++) {
-      String shortName = filenames.get(i); 
+      String shortName = withoutExts.get(i);
       int indDash = shortName.lastIndexOf("-");
       if (indDash < 0 || indDash == (shortName.length() - 1)) {
          continue;
@@ -627,8 +657,38 @@ static int getMaxVersion(L<String> filenames) {
    return result;
 }
 
+static <X> void print(X x) {
+   System.out.println(x);
+}
+
+//}}}
+//{{{ Blog
+
+static class Blog {
+   FileSys fs;
+
+   public Blog(FileSys fs)  {
+      this.fs = fs;
+   }
+
+   void ingestCore() {
+      String dir = Paths.get(ingestDir, coreSubfolder).toString();
+      if (!fs.dirExists(dir))  {
+         return;
+      }
+      var files = fs.listFiles(dir);
+      for (FileInfo fi : files) {
+         if (coreFiles.findIndex(x -> x.equals(fi.name)) > -1) {
+            fs.moveFileToNewVersion(dir, fi.name, blogDir);
+         }
+      }
+   }
+}
+
 //}}}
 //{{{ Tests
+
+//{{{ Internal tests
 
 static class TestResults {
    int countRun;
@@ -639,7 +699,6 @@ static class TestResults {
    }
 }
 
-
 static void testSaveFile() {
    FileSys fs = new MockFileSys();
    boolean resSave = fs.saveOverwriteFile("a/b", "myFile.txt", "An ode to joy");
@@ -649,7 +708,7 @@ static void testSaveFile() {
 
 static void testFilePrefixes() {
    FileSys fs = new MockFileSys();
-   fs.saveOverwriteFile("a/b", "myFile.txt", "An ode to joy");
+   fs.saveOverwriteFile("a/b", "myFile.txt",   "An ode to joy");
    fs.saveOverwriteFile("a/b", "myFile-2.txt", "An ode to joy 2");
    fs.saveOverwriteFile("a/b", "myFile-3.txt", "An ode to joy 3");
    var versions = fs.getNamesWithPrefix("a/b", "myFile");
@@ -661,16 +720,37 @@ static void testMaxVersion() {
    blAssert(mv == 3);
 }
 
+//}}}
+
+static void testIngestCore() {
+   var fs = new MockFileSys();
+   String inDir = getIngestDir();
+   print("saving to dir " + inDir);
+   fs.saveOverwriteFile(inDir, "core.js", "core script");
+   fs.saveOverwriteFile(inDir, "core.css", "core styles");
+   Blog b = new Blog(fs);
+   b.ingestCore();
+   var coreFiles = fs.listFiles(blogDir);
+   var coreNames = coreFiles.trans(x -> x.name);
+   print("core names");
+   for (String a : coreNames) {
+      print(a);
+   }
+   assertArrsEqual(coreFiles.trans(x -> x.name), L.of("core.js", "core.css"));
+}
+
+
 public static void main(String[] args) {
    System.out.println("Hw");
    TestResults counters = new TestResults();
-   runTest(Test::testSaveFile, counters);
-   runTest(Test::testFilePrefixes, counters);
-   runTest(Test::testMaxVersion, counters);
+//~   runTest(Test::testSaveFile, counters);
+//~   runTest(Test::testFilePrefixes, counters);
+//~   runTest(Test::testMaxVersion, counters);
+   runTest(Test::testIngestCore, counters);
    if (counters.countFailed > 0)  {
-      System.out.println("Failed " + counters.countFailed + " tests"); 
+      System.out.println("Failed " + counters.countFailed + " tests");
    } else {
-      System.out.println("Successfully run " + counters.countRun + " tests"); 
+      System.out.println("Successfully run " + counters.countRun + " tests");
    }
 }
 
